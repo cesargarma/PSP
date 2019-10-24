99aee4c32a914a2c6c1f564bc5927c0c4594e5d4
cesar <cesyser@gmail.com>
2019-10-24 10:23:12 +0200

    Signal 1 Funcional

--- SIGNALS/signal1.c
+++ SIGNALS/signal1.c
@@ -7,12 +7,18 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
+void funcion();
+void noMuere();
+void muere();
+
 void main() {
   int fd1[2];
   pipe (fd1);
   pid_t pidPadre = getpid();
 
   int valorFork = fork();
+
+  signal(SIGINT, noMuere);
 
   switch(valorFork){
     case -1:
@@ -21,13 +27,37 @@ void main() {
 
     case 0:
         //Estamos en el hijo
-
+        //signal(SIGINT, noMuere);
+        while(1){
+          //ENVIAMOS SEÑAl
+          sleep(3);
+          kill(getppid(), SIGUSR1);
+        }
         break;
     default:
         //Estamos en el padre
+        signal(SIGUSR1, funcion);
+        //signal(SIGINT, noMuere);
         while(1){
-          printf("%s\n", "Esperando...");
-          sleep(1);
+            //ESPERANDO SEÑAL
+            printf("%s\n", "Esperando...");
+            sleep(1);
         }
+        funcion();
     }
+}
+
+
+void funcion (){
+    printf("Jaja, te interrumpo, soy %d \n", getpid());
+}
+
+void noMuere(){
+    printf("Pues no me da la gana morirme, por ceirto, soy %d\n", getpid());
+    signal(SIGINT, muere);
+}
+
+void muere(){
+    printf("%s\n", "Bueno, igual si que me muero");
+    exit(1);
 }
